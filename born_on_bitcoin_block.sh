70ee32435fc39a612c71288b616fda89aaae45f8
Epic Curious <109078515+epiccurious@users.noreply.github.com>
2022-12-30 00:41:08 -0500

    Create born_on_bitcoin_block.sh

---
+++ born_on_bitcoin_block.sh
@@ -0,0 +1,31 @@
+target_time=1234567890
+
+block_count=$(~/bitcoin/bin/bitcoin-cli getblockcount)
+
+current_block=0
+current_block_hash=$(~/bitcoin/bin/bitcoin-cli getblockhash $current_block)
+current_block_time=0
+
+while [ $current_block -le $block_count ] && [ $current_block_time -lt $target_time ]; do
+  current_block_header=$(~/bitcoin/bin/bitcoin-cli getblockheader $current_block_hash)
+  current_block_time=$(echo $current_block_header | jq -r '.time')
+  current_block_difficulty=$(echo $current_block_header | jq -r '.difficulty')
+
+  echo "--------"
+  echo "The current block is $current_block"
+  echo "Its difficulty is $current_block_difficulty."
+  echo "It was mined on $(date -d @$current_block_time), or $current_block_time."
+
+  if [ $current_block_time -ge $target_time ]; then
+    echo "You were born on block $current_block, mined on $(date -d @$current_block_time)."
+    echo "Its hash is $current_block_hash"
+    echo "The block difficulty is $$(echo $current_block_header | jq -r '.difficulty')."
+  else
+    if [ $current_block -eq $block_count ]; then
+      echo "You were born in the future -_-"
+    else
+      current_block=$(($current_block+1))
+      current_block_hash=$(echo $current_block_header | jq -r '.nextblockhash')
+    fi
+  fi
+done
